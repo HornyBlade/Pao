ab39a73f726b48134ef1a796e6f25daf3feffc5f
HornyBlade <gotpover_1732@mail.ru>
2025-02-05 15:09:26 +0300

    Add files via upload

---
+++ prak1.py
@@ -0,0 +1,23 @@
+#import cryptocode
+
+str1='coldplay or radiohead'
+a1=str1[0]
+a2=str1[-1]
+a3=str1[2]
+a4=str1[-3]
+print(a1,a2,a3, a4)
+
+length= len(str1)
+print(length)
+
+str2= 'Too old to die young'
+length1=len(str2)
+print(length1)
+b1=str2[0:7]
+
+b2=str2[7:11]
+b3=str2[3]+str2[6]+str2[9]+str2[12]+str2[15]+str2[18]
+print(b1,b2,b3)
+#s1= cryptocode.encrypt('Timofey', 'student')
+#s2= cryptocode.decrypt(s1, 'student')
+#print(s2)
